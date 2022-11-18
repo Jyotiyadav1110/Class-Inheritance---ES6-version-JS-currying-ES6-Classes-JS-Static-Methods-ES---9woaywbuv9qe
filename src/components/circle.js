df61cafd98c5368fc095e30eb1fdc1d48b495378
Jyoti Yadav <82959912+Jyotiyadav1110@users.noreply.github.com>
2022-11-19 00:41:25 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,13 @@
 
 
 // export class using module.exports
+
+const Shape = require('./shape')
+
+class Circle extends Shape {
+    calculateArea() {
+        return 3.14;
+    }
+
+}
+module.exports = Circle
